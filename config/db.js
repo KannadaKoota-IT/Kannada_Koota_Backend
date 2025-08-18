9db0e93e75e53320590180630f4ce7e5aa83b3dd
Shreyas-J-U <sshreyasju@gmail.com>
2025-08-18 17:57:41 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -16,27 +16,3 @@ const connectDB = async () => {
 };
 
 export default connectDB;
-
-// config/db.js
-// import mongoose from "mongoose";
-
-// const connectDB = async () => {
-//   try {
-//     const mongoURI = process.env.MONGO_URI;
-//     console.log("🔍 MONGO_URI from env:", mongoURI); // Debugging
-
-//     if (!mongoURI) {
-//       throw new Error("❌ MONGO_URI not found in environment variables");
-//     }
-
-//     const conn = await mongoose.connect(mongoURI);
-
-//     console.log(`✅ MongoDB connected: ${conn.connection.host}`);
-//   } catch (err) {
-//     console.error("❌ MongoDB connection error:", err.message);
-//     process.exit(1);
-//   }
-// };
-
-// export default connectDB;
-
